6730095563c78a63325a771f904b9c649e7892cc
Kan201202 <kan201202@gmail.com>
2025-09-27 10:35:01 +0530

    duplicate files

--- src/pages/History.jsx
+++
@@ -1,3 +0,0 @@
-export default function History() {
-   return <div className="p-4 text-gray-700">History page (coming soon)</div>;
-}
--- src/utils/storage.js
+++
@@ -1,7 +0,0 @@
-export const tokenKey = "vv_token";
-
-export const storage = {
-  get: () => localStorage.getItem(tokenKey),
-  set: (t) => localStorage.setItem(tokenKey, t),
-  clear: () => localStorage.removeItem(tokenKey),
-};
